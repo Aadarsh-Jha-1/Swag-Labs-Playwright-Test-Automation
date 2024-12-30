76df7c79801b8de1ac250b822c2f32320821fbe8
Aadarsh Jha <aadarsh.jha@gamemano.in>
2024-12-30 15:19:05 +0530

    test cases for nav

--- Test-Cases/inventory.spec.js
+++ Test-Cases/inventory.spec.js
@@ -8,7 +8,7 @@ let page, context;
 
 test.describe('set test cases for Inventory Page', () => {
 
-    let producItem, sortingDropdown;
+    let producItem, sortingDropdown, navBtn;
 
     
     test.beforeEach(async ({ browser }) => {
@@ -21,9 +21,12 @@ test.describe('set test cases for Inventory Page', () => {
 
         sortingDropdown = page.locator('[data-test="product-sort-container"]');
 
+        navBtn = page.locator('#react-burger-menu-btn');
+
        
     });
 
+    // Number of Projects on invetory Page
     
     test('Validate total number of products on inventory page', async () => {
 
@@ -34,6 +37,9 @@ test.describe('set test cases for Inventory Page', () => {
         await expect(producItem).toHaveCount(6);
 
     });
+
+
+    // Sorting of elements on inventory page
 
     test('Validate sorting Name (A To Z)', async () => {
 
@@ -108,6 +114,142 @@ test.describe('set test cases for Inventory Page', () => {
         await page.waitForTimeout(3000);
 
     });
+
+
+    // Hamburger Button or Naviagtion Button On top Left
+
+
+
+test('Navigation Bar Redirections - All Items', async () => {
+
+        await login(page, 'standard_user', 'secret_sauce');
+
+        await expect(page).toHaveTitle('Swag Labs');
+
+        await page.getByText('Sauce Labs Backpack').click();
+
+        await page.waitForTimeout(3000);
+
+        //validate you are on the correct product page.
+
+        await expect(page).toHaveURL("https://www.saucedemo.com/inventory-item.html?id=4");
+
+       // Test All Items Button  In Nav Bar
+
+        await navBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        const allItemsBtn = page.locator('#inventory_sidebar_link');
+
+        await allItemsBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        // validate you are on inventory page now which have 6 product items
+
+        await expect(producItem).toHaveCount(6);
+
+
+
+    });
+
+
+    test('Navigation Bar Redirections - About', async () => {
+
+        await login(page, 'standard_user', 'secret_sauce');
+
+        await expect(page).toHaveTitle('Swag Labs');
+
+       // Test About Button  In Nav Bar
+
+        await navBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        const aboutBtn = page.locator('#about_sidebar_link');
+
+        await aboutBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        // validate you are on The About Page
+
+       await expect(page).toHaveTitle('Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing');
+
+
+    });
+
+
+
+    test('Navigation Bar Redirections - Logout', async () => {
+
+        await login(page, 'standard_user', 'secret_sauce');
+
+        await expect(page).toHaveTitle('Swag Labs');
+
+       // Test About Button  In Nav Bar
+
+        await navBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        const logoutBtn = page.locator('#logout_sidebar_link');
+
+        await logoutBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        // validate you are on The About Page
+
+       await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+
+    });
+
+
+    test('Navigation Bar Redirections - Reset App State', async () => {
+
+        await login(page, 'standard_user', 'secret_sauce');
+
+        await expect(page).toHaveTitle('Swag Labs');
+
+
+        // Change the App State - Add Some product in the cart.
+
+
+        const backpackAddToCart = page.locator('#add-to-cart-sauce-labs-backpack');
+
+        await backpackAddToCart.click();
+
+        const bikeLightAddToCart = page.locator('#add-to-cart-sauce-labs-bike-light');
+
+        await bikeLightAddToCart.click();
+
+       // Test About Button  In Nav Bar
+
+        await navBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        const resetBtn = page.locator('#reset_sidebar_link');
+
+        await resetBtn.click();
+
+        await page.waitForTimeout(2000);
+
+        // validate you are on The About Page
+
+       await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+
+    });
+
+
+
+
+
+
 
 
 
