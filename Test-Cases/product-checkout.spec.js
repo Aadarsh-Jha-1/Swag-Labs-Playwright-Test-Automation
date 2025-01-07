c09ff6c6c1defe46d7bfa86b1136abcec0f86a73
Aadarsh Jha <aadarshjhaofficials@gmail.com>
2025-01-07 08:58:17 +0530

    Added Assertions In Product Checkout Flow

--- Test-Cases/product-checkout.spec.js
+++ Test-Cases/product-checkout.spec.js
@@ -51,15 +51,41 @@ test('Successful checkout flow', async () => {
 
     await page.locator('#checkout').click();
 
+
+
+    // Checkout: Your Information Page Assertions
+
     await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-one.html');
+
+    await page.locator('#continue').click();
+
+    const requiredFieldError = page.getByText('Error: First Name is required');
+
+    await expect(requiredFieldError).toBeVisible();
 
     await page.getByPlaceholder('First Name').fill('Test');
 
+    await page.locator('#continue').click();
+
+    const requiredFieldError2 = page.getByText('Error: Last Name is required');
+
+    await expect(requiredFieldError2).toBeVisible();
+
     await page.getByPlaceholder('Last Name').fill('Automation');
+
+    await page.locator('#continue').click();
+
+    const requiredFieldError3 = page.getByText('Error: Postal Code is required');
+
+    await expect(requiredFieldError3).toBeVisible();
 
     await page.getByPlaceholder('Zip/Postal Code').fill('123456');
 
     await page.locator('#continue').click();
+
+    
+    
+    // Checkout: Overview page Assertions
 
     await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-two.html');
 
@@ -67,7 +93,54 @@ test('Successful checkout flow', async () => {
 
     await expect(saucelabBikeLight).toBeVisible();
 
+
+    //Extract Price 1
+
+    const item1PriceLocator = page.locator("body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1)");
+
+    const item1Price = await item1PriceLocator.textContent();
+
+    const extractPriceContent = item1Price.slice("1"); // remove dollar sign
+
+    const price1 = parseFloat(extractPriceContent); //  convert string to floating number
+
+
+    //Extract price 2 
+
+    const item2PriceLocator = page.locator("body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1)");
+
+    const item2Price = await item2PriceLocator.textContent();
+
+    const extractPriceContent2 = item2Price.slice("1"); // remove dollar sign
+
+    const price2 = parseFloat(extractPriceContent2); //  convert string to floating number
+
+    // Total Price
+
+    const actualTotalPrice = price1+price2;
+
+
+    // Visble Total Price Extractor
+
+    const totalPriceLocator = page.locator("//div[@class='summary_subtotal_label']");
+
+    const totalPrice = await totalPriceLocator.textContent();
+
+    console.log(totalPrice);
+
+    const extractPriceContent3 = totalPrice.slice("13"); // remove dollar sign
+
+    const visibleTotalPrice = parseFloat(extractPriceContent3); //  convert string to floating number
+
+    console.log(visibleTotalPrice);
+
+    expect(visibleTotalPrice).toEqual(actualTotalPrice);
+
     await page.locator('#finish').click();
+
+   
+   
+    //Thank You Page Assertions
 
     await  expect(page).toHaveURL('https://www.saucedemo.com/checkout-complete.html');
 
